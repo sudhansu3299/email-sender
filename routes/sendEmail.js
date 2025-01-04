aa5b421dcee407132ca1ea04f654f0a8ec976765
sudhansu swain <sudhansu.swain@oracle.com>
2025-01-05 02:08:29 +0530

    first commit

---
+++ routes/sendEmail.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey('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');
+
+const app = express();
+app.use(cors());  // Enable CORS
+app.use(express.json());
+
+app.post('/api/sendEmail', async (req, res) => {
+    const msg = {
+        to: 'sudhansu3299@gmail.com',
+        from: 'ransomguy69@gmail.com',
+        subject: 'Date Night',
+        text: 'It is a yes!',
+    };
+
+    try {
+        await sgMail.send(msg);
+        res.status(200).send('Email sent!');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Failed to send email');
+    }
+});
+
+// Start the server
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server running');
+});
